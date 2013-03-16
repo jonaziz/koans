e7d096c1e6610fad7b96449f70391a70a14c97cc
Jon Aziz <jonaziz@gmail.com>
2013-03-16 16:27:09 -0400

    Adding triangle stage 2

--- triangle.rb
+++ triangle.rb
@@ -14,13 +14,21 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+
+  if a <= 0 || b <= 0 || c <= 0
+ 	raise TriangleError, "All sides must be greater than 0"
+  elsif (a + b <= c) || (a + c <= b)|| (b + c <= a)
+  	raise TriangleError, "Shape is impossible"
+  end  		
+
   if a == b && a == c && b == c
   	:equilateral
   elsif a == b || a == c || b == c
   	:isosceles
   else
   	:scalene
-  end  		
+  end  
+
 end
 
 # Error class used in part 2.  No need to change this code.
